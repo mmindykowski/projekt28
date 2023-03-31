22993276e46a8dba4fe86ffb1a6fdd92190ee311
Mikolaj Mindykowski <mikolaj.mindykowski@gmail.com>
2023-03-31 17:44:55 +0200

    sprawdzanie liczby

---
+++ main.js
@@ -0,0 +1,11 @@
+function numbers (n, m) {
+    if ( (n == 20) || (m == 20) || n + m <= 20) {
+        return "True";
+  }  else {
+        return "False";
+  }
+}
+
+
+console.log(numbers(20, 1));
+
